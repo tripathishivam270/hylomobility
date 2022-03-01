0ed6bf275c8a705f88fae94c970555b6ab2e67b4
tripathishivam270 <100338222+tripathishivam270@users.noreply.github.com>
2022-03-02 01:09:45 +0530

    Add files via upload

---
+++ news.php
@@ -0,0 +1,44 @@
+<?php include('header.php')?>		 
+<div class="container-fluid">
+   <!-- DataTables Example -->
+   <div class="card mb-3">
+	  <div class="card-header">
+		 <i class="fa fa-fw fa-newspaper"></i>
+		 News Table
+	  </div>
+	  <div class="card-body">
+		 <div class="table-responsive">
+			<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+			   <thead>
+				  <tr>
+					 <th>Title</th>
+					 <th>Description</th>
+					 <th>Date</th>
+				  </tr>
+			   </thead>
+			   <tfoot>
+				  <tr>
+					 <th>Title</th>
+					 <th>Description</th>
+					 <th>Date</th>
+				  </tr>
+			   </tfoot>
+			   <tbody>
+				  <tr>
+					 <td>Nullam sem dolor, ultrices a sapien ut, blandit ferm</td>
+					 <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lacinia neque a scelerisque varius. Mauris consequat, massa vel tristique viverra, mi mi mattis magna, eu sodales augue nunc eget nulla. Cras consequat arcu sed imperdiet placerat. Nunc ornare sit amet ligula eu interdum. Nunc felis tellus, volutpat ut nulla a, tincidunt euismod tortor. Praesent eget elit at ligula porta euismod. Integer quis enim quis mi ultricies viverra. Integer neque dolor, suscipit non congue vitae, lobortis egestas urna. Aliquam velit sem, eleifend vitae elit et, blandit fermentum metus. Mauris id massa ut justo iaculis pulvinar sit amet eget odio. Vestibulum quis aliquet orci. Praesent nec suscipit erat. Vivamus malesuada ullamcorper sapien, ut fermentum tortor eleifend nec. Vivamus lectus libero, faucibus nec enim eget, vestibulum venenatis dolor. Morbi et enim neque. Interdum et malesuada fames ac ante ipsum primis in faucibus.</td>
+					 <td>12th Sep 2019</td>
+				  </tr>
+				  <tr>
+					 <td>Lorem ipsum dolor sit</td>
+					 <td>Sed egestas eros lorem, sed congue orci dapibus sed. Nullam ut pulvinar mi. Duis auctor lorem est. Nulla quis ante risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas lobortis sagittis sapien, sit amet molestie nisl elementum a. Fusce a vehicula eros. Integer enim ipsum, faucibus id feugiat vitae, finibus eu turpis.</td>
+					 <td>14th Sep 2019</td>
+				  </tr>
+			   </tbody>
+			</table>
+		 </div>
+	  </div>
+   </div>
+</div>
+<!-- /.container-fluid -->
+<?php include('footer.php')?>
